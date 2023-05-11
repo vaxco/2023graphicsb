7c0d74f06313067d704c7760899274613e51c200
vaxco <Johnson030813@gmail.com>
2023-05-11 16:11:13 +0800

    Week13

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -1,15 +1,39 @@
 ///Final_Project
 #include <stdio.h>
 #include <GL/glut.h>
+#include "glm.h"
+
+GLMmodel * head = NULL;
+GLMmodel * body = NULL;
+GLMmodel * ruparm = NULL;
+GLMmodel * rlowarm = NULL;
+int show[4] = {1,0,0,0};
+
 float teapotX = 0, teapotY = 0;
 FILE * fout = NULL;
 FILE * fin = NULL;
+void keyboard(unsigned char key, int x,int y){
+    if(key== '0') show[0] = ! show[0];
+    if(key== '1') show[1] = ! show[1];
+    if(key== '2') show[2] = ! show[2];
+    if(key== '3') show[3] = ! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL){
+        head = glmReadOBJ("model/head.obj");
+        body = glmReadOBJ("model/body.obj");
+        ruparm = glmReadOBJ("model/ruparm.obj");
+        rlowarm = glmReadOBJ("model/rlowarm.obj");
+    }
     glPushMatrix();
-        glTranslatef(teapotX, teapotY, 0);
-        glutSolidTeapot( 0.3 );
+        glScalef(0.3,0.3,0.3);
+        if(show[0]) glmDraw(head, GLM_MATERIAL);
+        if(show[1]) glmDraw(body,GLM_MATERIAL);
+        if(show[2]) glmDraw(ruparm,GLM_MATERIAL);
+        if(show[3]) glmDraw(rlowarm,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -24,20 +48,20 @@ void mouse(int button, int state, int x, int y)
     }
     display();
 }
-void keyboard(unsigned char key, int x, int y)
-{
-    if(fin==NULL){
-        fclose(fout);
-        fin = fopen("file4.txt", "r");
-    }
-    fscanf(fin, "%f%f", &teapotX, &teapotY);
-    display();
-}
+///void keyboard(unsigned char key, int x, int y)
+///{
+///   if(fin==NULL){
+///        fclose(fout);
+///        fin = fopen("file4.txt", "r");
+///    }
+///    fscanf(fin, "%f%f", &teapotX, &teapotY);
+///    display();
+///}
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
-    glutCreateWindow("week12");
+    glutCreateWindow("week13");
 
     glutDisplayFunc(display);
     glutKeyboardFunc(keyboard);
